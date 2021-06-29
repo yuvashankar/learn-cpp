c928f9e6c3c4c3931aeb63efb779c106e2b2c361
Vinay Yuvashankar <vinay.yuvashankar@gmail.com>
2021-06-29 09:01:30 -0400

    day_8 lessons complete

---
+++ week_2/src/day_8/e_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::uint16_t yourAge = 0;
+    std::uint16_t * pYourAge = &yourAge;
+
+    std::cout << "yourAge: " << yourAge << "\t pYourAge: " << pYourAge << std::endl;
+    * pYourAge = 50;
+    
+    std::cout << "yourAge: " << yourAge << "\t pYourAge: " << pYourAge << std::endl;
+}
